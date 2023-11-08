d5b0389c6ba94593da69e4fb65d457b2d96260b0
Selçuk Şan <56341239+selcuksan@users.noreply.github.com>
2023-11-08 22:21:52 +0300

    Create producer.sh

---
+++ producer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function generate_username() {
+  # Alfanumerik karakterlerden oluşan bir dizi oluşturun
+  CHARS="abcdefghijklmnopqrstuvwxyz0123456789"
+  USERNAME=""
+  USERNAME=$(echo "${CHARS:$(( RANDOM % ${#CHARS} )):10}")
+  echo $USERNAME
+}
+
+# Kafka broker hostname veya IP adresini değiştirin
+KAFKA_BROKER="localhost:9092"
+# Kafka konu başlığı adını değiştirin
+TOPIC_NAME="topic-1"
+# Kullanıcı adını Kafka konusuna gönderin
+for i in {1..100}; do
+  # Kullanıcı adı oluşturmak için fonksiyonu çağırın
+  USERNAME=$(generate_username)
+  echo "$USERNAME" | kafka-console-producer --topic $TOPIC_NAME --bootstrap-server $KAFKA_BROKER
+done
